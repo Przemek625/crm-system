e544e5c9f45f8460a78878a42ea5e4fb530b45ec
przemek625 <przemek625@gmail.com>
2017-11-01 14:48:55 +0100

    refactoring code

--- users/views.py
+++ users/views.py
@@ -20,7 +20,6 @@ class LoginView(View):
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
-            # TODO handle fail of authentication
             user = authenticate(username=username, password=password)
             if user:
                 login(request, user)
